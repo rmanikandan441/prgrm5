d463893f5d6032880aaf5aee4e0698a37b053a56
rmanikandan441 <42027847+rmanikandan441@users.noreply.github.com>
2019-01-24 12:21:12 +0530

    Update prgrm10.py

--- prgrm10.py
+++ prgrm10.py
@@ -1,6 +1,6 @@
-n=int(input("Enter number:"))
+a=int(input("Enter number:"))
 count=0
-while(n>0):
+while(a>0):
     count=count+1
-    n=n//10
+    a=a//10
 print("The number of digits in the number are:",count)
